b14a4a286084c7a65e904dffeb4f90f6cf22f4f7
bogdanovs <sergey.bogdanov@geropharm.com>
2020-04-08 23:01:38 +0300

    release

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', function(request, response) {
   response.send('hello itmo');
 })
 
+app.get('e2e-001.js', function(request, response) {
+  response.sendFile(path.join(__dirname, '/e2e-001.js'))
+})
+
 app.get('/e2e-001_', function(request, response) {
   response.sendFile(path.join(__dirname, '/index.html'))
 })
